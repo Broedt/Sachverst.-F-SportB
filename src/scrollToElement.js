a20e10287641cb7dabe538c8b05473bcf5208201
Broedt <164227660+Broedt@users.noreply.github.com>
2025-01-10 09:46:35 +0100

    fixed scrolling, when clicking navbar elements

--- src/scrollToElement.js
+++ src/scrollToElement.js
@@ -2,28 +2,33 @@
 
 export default function scrollToElement (liId) {
     let target = null;
+    const offset = window.innerHeight * 0.15;
+    let targetPosition = null;
     switch (liId){
         case 'li-0':
             target = document.getElementById('infoContent');
+            target.scrollIntoView({behavior: 'smooth'})
             break;
         case 'li-1':
             target = document.getElementById('serviceContent');
-            break;
+            targetPosition = target.getBoundingClientRect().top + window.scrollY;
+
+                window.scrollTo({
+                top: targetPosition - offset,
+                behavior: 'smooth'
+                });
+                break;
         case 'li-2':
             target = document.getElementById('about');
-    }
-    
-    
-  target.scrollIntoView({ behavior: 'smooth' })
+            targetPosition = target.getBoundingClientRect().top + window.scrollY;
 
-};
+                window.scrollTo({
+                top: targetPosition - offset,
+                behavior: 'smooth'
+                });
+    // still need to add case li-3 as soon as contetn for kontakt is created
     
-//if (targetElement) {
-//    const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;
- //   const offset = window.innerHeight * 0.15; // 15vh as offset
+  
 
-    // Scroll to the target position minus the offset
- //   window.scrollTo({
- //       top: targetPosition - offset,
- //       behavior: 'smooth'
- //   });
+    }};
+ 
